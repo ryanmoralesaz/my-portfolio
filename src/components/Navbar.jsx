5461972f5bab6bea3fe0d0c370864b9f43779941
git <ryanmoralesaz@gmail.com>
2025-06-03 15:46:55 -0700

    navbar built

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,38 @@
-import {useEffect} from 'react';
+import { useEffect } from "react";
 
-export const Navbar = ({menuOpen, setMenuOpen}) => {
-  useEffect(()=>{
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
-  },[menuOpen]);
+  }, [menuOpen]);
   return (
-    <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(var(--vanilla)/1)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="#home" className="font-mono text-xl font-bold text-white">
-            ryan<span className="text-blue-500">.tech</span>
+          <a
+            href="#home"
+            className="font-michroma text-xl font-bold text-flame">
+            Home
           </a>
-          <div className={`w-7 h-5 relative cursor-pointer z-40 md:hidden ${menuOpen ? "opacity-0" : "opacity-100"}`} onClick={()=>setMenuOpen((prev)=> !prev)}>
+          <div
+            className={`w-7 h-5 relative cursor-pointer z-40 md:hidden ${
+              menuOpen ? "opacity-0" : "opacity-100"
+            }`}
+            onClick={() => setMenuOpen(prev => !prev)}>
             &#9776;
           </div>
           <div className="hidden md:flex items-center space-x-8">
-            <a href="#home" className="text-gray-300 hover:text-white transition-colors">Home</a>
-            <a href="#about" className="text-gray-300 hover:text-white transition-colors">About</a>
-            <a href="#projects" className="text-gray-300 hover:text-white transition-colors">Projects</a>
-            <a href="#contacts" className="text-gray-300 hover:text-white transition-colors">Contacts</a>
+            <a href="#home" className="nav-link">
+              About Me
+            </a>
+            <a href="#about" className="nav-link">
+              Portfolio
+            </a>
+            <a href="#projects" className="nav-link">
+              Classroom Technology
+            </a>
           </div>
         </div>
       </div>
     </nav>
   );
-}
+};
